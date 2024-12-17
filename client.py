e0323768240e19f8c60255ceb211eb1a930d2c24
Justin Fanning <justinfanning3.14@gmail.com>
2024-12-17 08:41:41 -0500

    Create simple server with echo client

---
+++ client.py
@@ -0,0 +1,12 @@
+import os
+import socket 
+#Default IP for internal network setup using Virtual Box, connection made between Desktop and VM.
+HOST = os.getenv("HOST", "192.168.56.102")
+PORT = int(os.getenv("PORT", 8080))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
