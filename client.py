d48e962073fc3cef705393ea15c12ccda3cdf3bd
Justin Fanning <justinfanning3.14@gmail.com>
2024-12-17 08:41:41 -0500

    Add multi-client capability

--- client.py
+++ client.py
@@ -1,12 +1,70 @@
-import os
 import socket 
-#Default IP for internal network setup using Virtual Box, connection made between Desktop and VM.
-HOST = os.getenv("HOST", "192.168.56.102")
-PORT = int(os.getenv("PORT", 8080))
+import selectors
+import sys
+import types
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+"""
+Version 2.0 http server, multi-connection client using selector module.
+Client can connect mulitple sockets to server simultaniously, sends predefined messages, and
+reads server response.
+"""
 
-print(f"Received {data!r}")
+sel = selectors.DefaultSelector()
+messages = [b"Message 1", b"Message 2"] #The predetermined messages to be sent to the server.
+
+def start_connection(host, port, num_conns): #Method to start connection to the server.
+    server_addr = (host, port)
+    for i in range(0, num_conns):
+        connid = i + 1
+        print(f"Starting connection {connid} to {server_addr}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(
+            connid=connid,
+            msg_total=sum(len(m) for m in messages),
+            recv_total=0,
+            messages=messages.copy(),
+            outb=b""
+        )
+        sel.register(sock, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj #The socket being monitored.
+    data = key.data #Connection state.
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data: #If data is recieved prints it.
+            print(f"Received {recv_data!r} from connection {data.connid}")
+            data.recv_total += len(recv_data)
+        if not recv_data or data.recv_total == data.msg_total:
+            print(f"Closing connection {data.connid}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.message:
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print(f"Sending {data.outb!r} to connect {data.connid}")
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+
+if len(sys.argv) != 4: #Defines command line requirements.
+    print(f"Usage: {sys.argv[0]} <host> <port> <num_connections>")
+    sys.exit(1)
+host, port, num_conns = sys.argv[1:4]
+start_connection(host, int(port), int(num_conns))
+
+try: 
+    while True:
+        events = sel.select(timeout=1)
+        if events:
+            for key, mask in events:
+                service_connection(key, mask) #Handles all connections.
+        if not sel.get_map(): #When no sockets left loop breaks.
+            break
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
--- host.py
+++ host.py
@@ -1,18 +1,62 @@
-import os
-import socket 
-#Default IP for internal network setup using Virtual Box, connection made between Desktop and VM.
-HOST = os.getenv("HOST", "192.168.56.102")
-PORT = int(os.getenv("PORT", 8080))
+import socket
+import selectors
+import sys
+import types
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST,PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
-            
+"""
+Version 2.0 http server, multi-client echo server using selectors.
+"""
+
+sel = selectors.DefaultSelector()
+#When a new client connection is ready to be accepted.
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False) #sets blocking for the new socket object to be false.
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+#fucntion called when client is ready for reading/writing.
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024) #client data.
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+
+    if mask & selectors.EVENT_WRITE:
+        if data.outb: #If there is data to send.
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb) #Send data to client.
+            data.outb = data.outb[sent:] #Remove sent portion.
+
+if len(sys.argv) != 3: #Defines command line requirements.
+    print(f"Usage: {sys.argv[0]} <host> <port>")
+    sys.exit(1)
+
+host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind(host, port)
+lsock.listen()
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False) #Makes server socket non-blocking.
+sel.register(lsock, selectors.EVENT_READ, data=None) #Registers the server socket to monitor for EVENT_READ (new connection).
+
+try:
+    while True:
+        events = sel.select(timeout=None) #Wait for event.
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj) #Accepts new connection.
+            else:
+                service_connection(key, mask) #Handle client comms.
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
